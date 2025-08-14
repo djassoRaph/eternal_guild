b11c69d5584faa75b4f2946b06f18fb28bf9a9a4
Raphael Reck <raphaelreck@gmail.com>
2025-08-14 14:05:27 +0200

    Attempting new UI.

--- scripts/ContextPanel.gd
+++ scripts/ContextPanel.gd
@@ -1,1 +1,33 @@
-extends Node
+extends Control
+class_name ContextPanel
+
+@export var log_text_path: NodePath
+@export var next_day_button_path: NodePath
+
+var log_text: TextEdit
+var next_day_button: Button
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+	if log_text_path != NodePath():
+		log_text = get_node(log_text_path) as TextEdit
+	if next_day_button_path != NodePath():
+		next_day_button = get_node(next_day_button_path) as Button
+
+	if next_day_button:
+		next_day_button.pressed.connect(_on_next_day_pressed)
+
+	LogBus.message.connect(_on_log_message)
+	# Seed message so you see something immediately:
+	LogBus.post("UI online.")
+
+func _on_log_message(text: String) -> void:
+	if log_text:
+		if log_text.editable:
+			log_text.editable = false
+		log_text.append_text(text + "\n")
+		log_text.scroll_vertical = log_text.get_line_count()
+
+func _on_next_day_pressed() -> void:
+	LogBus.post("Next Day pressed.")
