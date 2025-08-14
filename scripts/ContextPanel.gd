ad129bd9b6eca7e9e6a61ff6ddabea3a795436b7
Raphael Reck <raphaelreck@gmail.com>
2025-08-14 15:10:50 +0200

    Its a long road.

--- scripts/ContextPanel.gd
+++ scripts/ContextPanel.gd
@@ -7,20 +7,18 @@ class_name ContextPanel
 var log_text: TextEdit
 var next_day_button: Button
 
+
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
-
 	if log_text_path != NodePath():
 		log_text = get_node(log_text_path) as TextEdit
 	if next_day_button_path != NodePath():
 		next_day_button = get_node(next_day_button_path) as Button
-
-	if next_day_button:
 		next_day_button.pressed.connect(_on_next_day_pressed)
-
 	LogBus.message.connect(_on_log_message)
-	# Seed message so you see something immediately:
+	LogBus.post("UI online.")  # so you see somethinge something immediately:
 	LogBus.post("UI online.")
+
 
 func _on_log_message(text: String) -> void:
 	if log_text:
--- scripts/TavernScreen.gd
+++ scripts/TavernScreen.gd
@@ -20,18 +20,25 @@ func _setup_textures() -> void:
 	for n in nodes:
 		n.set_anchors_preset(Control.PRESET_FULL_RECT)
 		n.set_offsets_preset(Control.PRESET_FULL_RECT)
-		n.expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL
-		n.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		n.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED  # fill + center
+		n.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 		n.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+		n.mouse_filter = Control.MOUSE_FILTER_IGNORE  # don't eat mouse input
+
+	hover_tavern.visible = false
+	hover_desk.visible = false
+
 	hover_tavern.visible = false
 	hover_desk.visible = false
 
 func _wire_zone(zone: Control, hover: TextureRect, zone_name: String) -> void:
 	zone.mouse_entered.connect(func() -> void:
 		hover.visible = true
+		LogBus.post("Hover: " + zone_name)
 	)
 	zone.mouse_exited.connect(func() -> void:
 		hover.visible = false
+		LogBus.post("Exit: " + zone_name)
 	)
 	zone.gui_input.connect(func(event: InputEvent) -> void:
 		if event is InputEventMouseButton:
